c63c549343666f1ac623f56285c0878ed9a452f8
neelyp <160043302+neelyp@users.noreply.github.com>
2024-03-26 20:29:52 +0000

    added a clear button to quickly clear the amount typed

--- app/src/components/Inputs/Text.jsx
+++ app/src/components/Inputs/Text.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Text = ({ getAmnt }) => {
   const [amnt, setAmnt] = useState(1);
@@ -6,17 +6,21 @@ const Text = ({ getAmnt }) => {
   useEffect(() => getAmnt(amnt), [amnt]);
 
   return (
-    <input
-      value={amnt}
-      onChange={(e) => {
-        const value = parseFloat(e.target.value);
+    <div className="amnt-text">
+      <input
+        value={amnt}
+        onChange={(e) => {
+          const value = parseFloat(e.target.value);
 
-        const new_ = isNaN(value) || (value === 0) === NaN ? 0 : value;
-        setAmnt(new_);
-      }}
-      type="text"
-      placeholder="Enter number of USD"
-    />
+          const new_ = isNaN(value) || (value === 0) === NaN ? 0 : value;
+          setAmnt(new_);
+        }}
+        type="text"
+        placeholder="Enter number of USD"
+      />
+
+      <input type="button" value="clear" onClick={() => setAmnt('')} />
+    </div>
   );
 };
 
